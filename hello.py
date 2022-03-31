3ba65661f966459acc2b1c299178f607355a7c3f
yu184743 <96573503+yu184743@users.noreply.github.com>
2022-03-31 20:14:16 +0900

    radio

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ st.title("hello world!")
 st.write("練習")
 st.text_input("   ")
 st.slider
-st.radio(label: str, options: Union[Sequence[Any], pandas.core.frame.DataFrame, pandas.core.series.Series, pandas.core.indexes.base.Index, numpy.ndarray], index: int = 0, format_func: Callable[[Any], Any] = <class 'str'>, key: Union[str, int, NoneType] = None, help: Union[str, NoneType] = None, on_change: Union[Callable[..., NoneType], NoneType] = None, args: Union[Tuple[Any, ...], NoneType] = None, kwargs: Union[Dict[str, Any], NoneType] = None, *, disabled: bool = False) -> Any
+st.radio(label:"練習" str, options: Union[Sequence[Any], pandas.core.frame.DataFrame, pandas.core.series.Series, pandas.core.indexes.base.Index, numpy.ndarray], index: int = 0, format_func: Callable[[Any], Any] = <class 'str'>, key: Union[str, int, NoneType] = None, help: Union[str, NoneType] = None, on_change: Union[Callable[..., NoneType], NoneType] = None, args: Union[Tuple[Any, ...], NoneType] = None, kwargs: Union[Dict[str, Any], NoneType] = None, *, disabled: bool = False) -> Any

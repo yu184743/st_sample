13eab5c98b23d2ddea993790058b87e2b8f90ee1
yu184743 <96573503+yu184743@users.noreply.github.com>
2022-03-31 20:05:46 +0900

    radio　練習

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("hello world!")
 st.write("練習")
 st.text_input("   ")
-st.radio(label:a str, options: Union[Sequence[Any], pandas.core.frame.DataFrame, pandas.core.series.Series, pandas.core.indexes.base.Index, numpy.ndarray], index: int = 0, format_func: Callable[[Any], Any] = <class 'str'>, key: Union[str, int, NoneType] = None, help: Union[str, NoneType] = None, on_change: Union[Callable[..., NoneType], NoneType] = None, args: Union[Tuple[Any, ...], NoneType] = None, kwargs: Union[Dict[str, Any], NoneType] = None, *, disabled: bool = False) -> Any
+st.radio(label:あああああ str, options: Union[Sequence[Any], pandas.core.frame.DataFrame, pandas.core.series.Series, pandas.core.indexes.base.Index, numpy.ndarray], index: int = 0, format_func: Callable[[Any], Any] = <class 'str'>, key: Union[str, int, NoneType] = None, help: Union[str, NoneType] = None, on_change: Union[Callable[..., NoneType], NoneType] = None, args: Union[Tuple[Any, ...], NoneType] = None, kwargs: Union[Dict[str, Any], NoneType] = None, *, disabled: bool = False) -> Any

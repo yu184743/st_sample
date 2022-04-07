1719a78ad382c1348b86d69e8b7360c3130b5293
yu184743 <96573503+yu184743@users.noreply.github.com>
2022-04-07 19:15:06 +0900

    kkkkkkkkkkkkk

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("hello world!")
 st.write("練習")
 st.text_input("   ")
-st.slider(label: str, min_value=None, max_value=None, value=None, step=None, format=None, key: Union[str, int, NoneType] = None, help: Union[str, NoneType] = None, on_change: Union[Callable[..., NoneType], NoneType] = None, args: Union[Tuple[Any, ...], NoneType] = None, kwargs: Union[Dict[str, Any], NoneType] = None, *, disabled: bool = False)
+st.slider(label: 1str, min_value=None, max_value=None, value=None, step=None, format=None, key: Union[str, int, NoneType] = None, help: Union[str, NoneType] = None, on_change: Union[Callable[..., NoneType], NoneType] = None, args: Union[Tuple[Any, ...], NoneType] = None, kwargs: Union[Dict[str, Any], NoneType] = None, *, disabled: bool = False)
